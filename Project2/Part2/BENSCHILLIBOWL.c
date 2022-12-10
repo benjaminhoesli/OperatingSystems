9233dbab0102cd9825a38b600bd998f491b9f535
benjaminhoesli <73198998+benjaminhoesli@users.noreply.github.com>
2022-12-10 02:45:12 +0100

    Update BENSCHILLIBOWL.c

--- Project2/Part2/BENSCHILLIBOWL.c
+++ Project2/Part2/BENSCHILLIBOWL.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <time.h>
+#include<unistd.h>
 
 bool IsEmpty(BENSCHILLIBOWL* bcb);
 bool IsFull(BENSCHILLIBOWL* bcb);
@@ -10,7 +11,7 @@ void AddOrderToBack(Order **orders, Order *order);
 
 
 
-// help from Ujjwal
+// help from Ujjwal and Yoofi
 // worked together with Anirudd
 
 
@@ -77,43 +78,44 @@ void CloseRestaurant(BENSCHILLIBOWL* bcb) {
  *  - return the order number
  */
 int AddOrder(BENSCHILLIBOWL* bcb, Order* order) {
-    pthread_mutex_lock(&bcb->mutex);
-    int final=bcb->next_order_number;
-    if(!IsFull(bcb)){
-      order->order_number=bcb->next_order_number;
-      AddOrderToBack(&bcb->orders,order);
-      bcb->next_order_number+=1;
-      bcb->current_size+=1;
+    pthread_mutex_lock(&(bcb->mutex));
+    while(IsFull(bcb)){
+        pthread_cond_wait(&(bcb->can_add_orders),&(bcb->mutex));
+      }
+    if(bcb->orders==NULL){
+      bcb->orders=order;
+      order->next=NULL;
     }
     else{
-      while(IsFull(bcb)){
-        pthread_cond_wait(&bcb->can_add_orders,&bcb->mutex);
-      }
-      order->order_number=bcb->next_order_number;
-      AddOrderToBack(&bcb->orders,order);
-      bcb->current_size+=1;
-      bcb->next_order_number+=1;
+      AddOrderToBack(&(bcb->orders),order);
     }
-  pthread_cond_signal(&bcb->can_get_orders);
-  pthread_mutex_unlock(&bcb->mutex);
-  return final;
+    
+    order->order_number=bcb->next_order_number;
+    bcb->next_order_number+=1;
+    bcb->current_size+=1;
+    int final=order->order_number;
+    pthread_cond_broadcast(&(bcb->can_get_orders));
+    pthread_mutex_unlock(&(bcb->mutex));
+    return final;
 }
 
 /* remove an order from the queue */
 Order *GetOrder(BENSCHILLIBOWL* bcb) {
     pthread_mutex_lock(&bcb->mutex);
-    Order *order;
     while(IsEmpty(bcb)){
-      pthread_cond_wait(&bcb->can_get_orders,&bcb->mutex);
+      if(bcb->orders_handled==bcb->expected_num_orders){
+        pthread_mutex_unlock(&bcb->mutex);
+        return NULL;
+      }
+      pthread_cond_wait(&(bcb->can_get_orders), &(bcb->mutex));
     }
-    order= bcb->orders;
-    bcb->orders=bcb->orders->next;
-    order->next=NULL;
-    bcb->current_size-=1;
-    bcb->orders_handled-=1;
-    pthread_cond_signal(&bcb->can_add_orders);
+    Order *curr_order = bcb->orders;
+    bcb->orders = curr_order->next;
+    bcb->current_size -= 1;
+    bcb->orders_handled+=1;
+    pthread_cond_broadcast(&bcb->can_add_orders);
     pthread_mutex_unlock(&bcb->mutex);
-    return order;
+    return curr_order;
 }
 
 // Optional helper functions (you can implement if you think they would be useful)
@@ -138,4 +140,3 @@ void AddOrderToBack(Order **orders, Order *order) {
   curr->next=order;
   order->next=NULL;
 }
-
